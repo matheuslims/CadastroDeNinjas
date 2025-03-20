8cc390a11be0cf481cb8c4ca1bd0917a11c6b558
matheuslims <omatheusdasilvalima@gmail.com>
2025-03-20 12:16:13 -0300

    Utilizando Lombok para criar construtores automaticamente, getters e setters também

--- src/main/java/dev/java10x/CadastroDeNinjas/Missoes/MissoesModel.java
+++ src/main/java/dev/java10x/CadastroDeNinjas/Missoes/MissoesModel.java
@@ -2,9 +2,15 @@ package dev.java10x.CadastroDeNinjas.Missoes;
 
 import dev.java10x.CadastroDeNinjas.Ninjas.NinjaModel;
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+import java.util.List;
 
 @Entity
 @Table(name = "tb_missoes")
+@AllArgsConstructor
+@Data
 public class MissoesModel {
 
     @Id
@@ -12,39 +18,8 @@ public class MissoesModel {
     private Long id;
     private String nome;
     private String dificuldade;
-    private NinjaModel ninja;
+    // Uma missão para muitos ninjas
+    @OneToMany(mappedBy = "missoes")
+    private List<NinjaModel> ninjas;
 
-    public MissoesModel(){
-
-    }
-
-    public MissoesModel(Long id, String nome, String dificuldade) {
-        this.id = id;
-        this.nome = nome;
-        this.dificuldade = dificuldade;
-    }
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getNome() {
-        return nome;
-    }
-
-    public void setNome(String nome) {
-        this.nome = nome;
-    }
-
-    public String getDificuldade() {
-        return dificuldade;
-    }
-
-    public void setDificuldade(String dificuldade) {
-        this.dificuldade = dificuldade;
-    }
 }
