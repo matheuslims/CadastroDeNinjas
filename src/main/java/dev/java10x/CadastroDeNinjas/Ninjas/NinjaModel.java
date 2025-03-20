00f437706dfee53b42513dad875cb3f18fc8d48b
matheuslims <omatheusdasilvalima@gmail.com>
2025-03-20 11:51:43 -0300

    Organizando as pastas do projeto e Criando MissoesModel

--- src/main/java/dev/java10x/CadastroDeNinjas/Ninjas/NinjaModel.java
+++ src/main/java/dev/java10x/CadastroDeNinjas/Ninjas/NinjaModel.java
@@ -1,5 +1,6 @@
-package dev.java10x.CadastroDeNinjas;
+package dev.java10x.CadastroDeNinjas.Ninjas;
 
+import dev.java10x.CadastroDeNinjas.Missoes.MissoesModel;
 import jakarta.persistence.*;
 
 // Entity ele transforma uma classe em uma entidade do banco de dados
@@ -14,6 +15,7 @@ public class NinjaModel {
     private String nome;
     private String email;
     private int idade;
+    private MissoesModel missoes;
 
     public NinjaModel() {
 
